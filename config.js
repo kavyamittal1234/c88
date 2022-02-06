25888707ddd2ff9d341f97f356b3b5f1652bd8ff
kavyamittal1234 <68001757+kavyamittal1234@users.noreply.github.com>
2022-02-06 14:29:36 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBr_j9fHsEu26bt6nQZLkbCr63DJuNEqjM",
+    authDomain: "story-telling-app-fc153.firebaseapp.com",
+    databaseURL: "https://story-telling-app-fc153-default-rtdb.firebaseio.com",
+    projectId: "story-telling-app-fc153",
+    storageBucket: "story-telling-app-fc153.appspot.com",
+    messagingSenderId: "979735637084",
+    appId: "1:979735637084:web:474e807fbb02652e57ca99"
+  };
+  
+  // Initialize Firebase
+  //const app = initializeApp(firebaseConfig);
